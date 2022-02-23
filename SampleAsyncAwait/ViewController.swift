c17afc8013c347f0881dbd91734df0d14ec09c90
mocchan <hcmnorgir0808@yahoo.co.jp>
2022-02-23 15:55:42 +0900

    Initial Commit

---
+++ SampleAsyncAwait/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SampleAsyncAwait
+//
+//  Created by 岩本康孝 on 2022/02/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
